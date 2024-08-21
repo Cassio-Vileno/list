760bb0cf9aec163f432be22e03192eae7da9be75
Cassio-Vileno <cassiovileno@gmail.com>
2024-08-21 15:48:49 -0300

    style: :lipstick: correção de responsividade de css

--- src/views/EditTask.vue
+++ src/views/EditTask.vue
@@ -81,11 +81,7 @@ form > div {
 }
 
 form > div > input {
-<<<<<<< HEAD
   font-size: 1.1rem;
-=======
-  font-size: 0.813rem;
->>>>>>> e23a927cfc7efd4e159e6dd71efcc84c6f70ff32
   background-color: #fff;
   padding: 15px 20px;
   border: none;
@@ -98,11 +94,7 @@ form > div > textarea,
 }
 
 form > div > textarea {
-<<<<<<< HEAD
   font-size: 1.1rem;
-=======
-  font-size: 0.813rem;
->>>>>>> e23a927cfc7efd4e159e6dd71efcc84c6f70ff32
   height: 500px;
   background-color: #fff;
   padding: 15px 20px;
